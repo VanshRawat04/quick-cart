deb1eb634fad6d3ac6659a82d53c2f50cf52554d
VANSH RAWAT <vanshrawat@VANSHs-MacBook-Pro.local>
2025-06-17 19:47:55 +0530

    error fixed

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,11 +1,10 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserUpdation } from "@/config/inngest";
-
+import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "@/config/inngest";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion,
+    syncUserDeletion
   ],
 });
